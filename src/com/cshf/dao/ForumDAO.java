98767a68d89b765c73c6caea65163827c902716e
augustine <hypocrisyzhang@163.com>
2019-05-04 09:53:07 +0800

    修改DAO

--- src/com/cshf/dao/ForumDAO.java
+++ src/com/cshf/dao/ForumDAO.java
@@ -1,4 +1,20 @@
 package com.cshf.dao;
 
+import com.cshf.entity.Forum;
+
+import java.sql.ResultSet;
+
 public class ForumDAO {
+
+    private Forum wrapForumFromResultSet(ResultSet rs) {
+        Forum forum = new Forum();
+        try {
+            forum.setFid(rs.getInt("fid"));
+
+        } catch (Exception e) {
+
+        }
+
+        return forum;
+    }
 }
